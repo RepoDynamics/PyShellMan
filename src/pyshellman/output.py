13554ef6441bb0770dbb80bad1d82917a8cbc961
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-21 15:01:37 +0200

    update

--- src/pyshellman/output.py
+++ src/pyshellman/output.py
@@ -1,5 +1,7 @@
 from typing import NamedTuple as _NamedTuple
 
+from rich import text as _text, console as _console, panel as _panel, markdown as _markdown
+
 
 class ShellOutput(_NamedTuple):
     command: str
@@ -34,5 +36,24 @@ class ShellOutput(_NamedTuple):
             return f"Command failed with exit code {self.code}."
         return f"Command executed successfully."
 
+    def __rich__(self):
+        group = [
+            _markdown.Markdown(f"- **Command**: `{self.command}`"),
+            _markdown.Markdown(f"- **Executed**: {self.executed}"),
+        ]
+        if self.executed:
+            group.append(_markdown.Markdown(f"- **Exit Code**: {self.code}"))
+        if self.output:
+            output = self.output if not isinstance(self.output, str) else _text.Text.from_ansi(self.output)
+            group.append(_panel.Panel(output, title="Output"))
+        if self.error:
+            error = self.error if not isinstance(self.error, str) else _text.Text.from_ansi(self.error)
+            group.append(_panel.Panel(error, title="Error"))
+        out = _panel.Panel(
+            _console.Group(*group),
+            title="Shell Command Output",
+        )
+        return out
+
     def __str__(self):
-        return "\n".join([f"{key}: {value}" for key, value in self.details.items()])
+        return "\n".join([f"- {key}: {value}" for key, value in self.details.items()])
